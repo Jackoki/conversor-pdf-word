32741aa66af3503f215fe4095e89342ddf08c93a
Jackoki <kenji.inoue@hotmail.com>
2026-03-12 22:33:13 -0300

    Criação do converter e realização de primeiros testes

--- backend/src/main/java/converter/Converter.java
+++ backend/src/main/java/converter/Converter.java
@@ -2,11 +2,29 @@ package converter;
 
 import java.nio.file.Path;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Converter {
-    public void convert(Path pathInputArchive, Path pathOutputArchive){
-        
+    public void convert(Path pathInputArchive, Path pathOutputArchive) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("soffice", "--headless", "--convert-to", "docx", "--outdir", pathOutputArchive.toString(), pathInputArchive.toString());
+            executeProcess(processBuilder);
+        }
+
+        catch (Exception e){
+            throw new RuntimeException("Falha durante a conversão", e);
+        }
+    }
+
+    private void executeProcess(ProcessBuilder processBuilder) throws Exception {
+        Process process = processBuilder.start();
+        int exitCode = process.waitFor();
+
+        if(exitCode != 0) {                
+            throw new RuntimeException("Falha ao executar o processo de conversão");
+        }
     }
 }
